9dd3316186fa167945682339ba4b0c0d8446c188
Daniel <danieldev0353@outlook.com>
2020-09-01 16:42:14 +0300

    Category page

--- frontend/components/auth/Admin.js
+++ frontend/components/auth/Admin.js
@@ -5,8 +5,10 @@ import { isAuth } from '../../actions/auth'
 const Admin = ({ children }) => {
   useEffect(() => {
     if (!isAuth()) {
+      console.log('1')
       Router.push(`/signin`)
     } else if (isAuth()?.role !== 1) {
+      console.log('2')
       Router.push(`/`)
     }
   }, [])
